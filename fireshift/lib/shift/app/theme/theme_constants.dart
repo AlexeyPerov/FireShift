fabdc618f9bec0687c6b5e0cecf1ea61638b6d91
Alexey Perov <a.perov@casualgamesonline.com>
2021-05-14 14:43:27 +0300

    Redundant code removed

--- fireshift/lib/shift/app/theme/theme_constants.dart
+++ fireshift/lib/shift/app/theme/theme_constants.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
 
 const double kMinWebContainerWidth = 850;
-const kPrimaryColor = Color(0xFF979797);
-const kPrimaryLightColor = Color(0xFF64B5F6);
 const double systemTextScaleFactorOption = -1;
