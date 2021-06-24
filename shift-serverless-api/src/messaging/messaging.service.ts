bed8d463f4daafe597f52963f0651e562cfdd399
Alexey Perov <alvperov@gmail.com>
2021-06-24 22:07:06 +0300

    getMessages & addMessage finished

--- shift-serverless-api/src/messaging/messaging.service.ts
+++ shift-serverless-api/src/messaging/messaging.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { exception } from 'console';
 import { SupportThreadInfoModel } from 'src/shared/models/support-thread-info.model';
-import { SupportThreadContentsModel } from 'src/shared/models/support_thread_contents.model';
+import { SupportThreadContents, SupportThreadContentsModel } from 'src/shared/models/support_thread_contents.model';
 import { SupportMessage } from '../shared/models/support-message.model';
 
 @Injectable()
@@ -11,17 +10,15 @@ export class MessagingService {
     @InjectModel('SupportThreadInfo') private infoModel: SupportThreadInfoModel,
     @InjectModel('SupportThreadContents')
     private contentsModel: SupportThreadContentsModel,
-  ) {}
+  ) { }
 
   getUnreadMessagesCount(userId: string): number {
     return 0;
   }
 
-  getMessages(userId: string): SupportMessage[] {
-    return [
-      /*new SupportMessage({authorId: "1", contents: "Hi!", time: Date.now() } ),
-      new SupportMessage({authorId: "0", contents: "Whats up!", time: Date.now() } )*/
-    ];
+  async getMessages(userId: string): Promise<SupportMessage[]> {
+    const contents = await this.findThreadContents(userId);
+    return contents.messages;
   }
 
   async addMessage(threadOwnerId: string, messageAuthorId: string, contents: string): Promise<boolean> {
@@ -29,60 +26,87 @@ export class MessagingService {
       threadOwnerId: threadOwnerId,
     });
 
-    if (!foundThreadInfo) {     
+    if (!foundThreadInfo) {
       console.log("creating new thread for owner " + threadOwnerId);
-
-      const newThreadContents = {
-          messages: [
-            {
-              authorId: messageAuthorId,
-              contents: contents,
-              time: Date.now()
-            }
-          ]
-      };
-
-      var contentsId = "";
-
-      try {
-        const addedContents = await new this.contentsModel(newThreadContents);
-        await addedContents.save();          
-        contentsId = addedContents._id;        
-      } catch (e) {
-        console.log(e);
-        return false;
-      }
-
-      const newThreadInfo = {
-        project: "AoC",
-        threadOwnerId: threadOwnerId,
-        receiverId: "0",
-        starred: false,
-        archived: false,
-        unread: false,
-        subject: "Support Request",
-        updateTime: Date.now(),
-        preview: "",
-        contentsId: contentsId
-      };
-
-      try {
-        const addedInfo = await new this.infoModel(newThreadInfo);
-        await addedInfo.save();                  
-      } catch (e) {
-        console.log(e);
-      }
-
-      foundThreadInfo = await this.infoModel.findOne({
-        threadOwnerId: threadOwnerId,
-      });
-
-      if (!foundThreadInfo) {
-        throw new Error("Error creating new thread");
-      }
+      this.createNewThread(threadOwnerId, messageAuthorId, contents);
+    } else {
+      console.log("existing thread has been found");
+      await this.updateThread(threadOwnerId, messageAuthorId, contents);
     }
 
-    // var message = new SupportMessage({authorId: userId, contents: contents, time: Date.now()})
     return true;
+  }
+
+  private async createNewThread(threadOwnerId: string, messageAuthorId: string, contents: string) {
+    const newThreadContents = {
+      messages: [
+        {
+          authorId: messageAuthorId,
+          contents: contents,
+          time: Date.now()
+        }
+      ]
+    };
+
+    var contentsId = "";
+
+    const addedContents = await new this.contentsModel(newThreadContents);
+    await addedContents.save();
+    contentsId = addedContents._id;
+
+    const newThreadInfo = {
+      project: "AoC",
+      threadOwnerId: threadOwnerId,
+      receiverId: "0",
+      starred: false,
+      archived: false,
+      unread: false,
+      subject: "Support Request",
+      updateTime: Date.now(),
+      preview: contents.length >= 24 ? contents.substr(0, 24) : contents,
+      contentsId: contentsId
+    };
+
+    const addedInfo = await new this.infoModel(newThreadInfo);
+    await addedInfo.save();
+  }
+
+  private async updateThread(threadOwnerId: string, messageAuthorId: string, contents: string) {
+    await this.contentsModel.updateOne(
+      {
+        $push: {
+          messages: [{
+            authorId: messageAuthorId,
+            contents: contents,
+            time: Date.now()
+          }]
+        }
+      },
+    );
+
+    await this.infoModel.updateOne(
+      {
+        preview: contents.length >= 24 ? contents.substr(0, 24) : contents,
+        updateTime: Date.now()
+      }
+    );
+  }
+
+  private async findThreadContents(userId: string): Promise<SupportThreadContents> {
+    const foundThreadInfo = await this.infoModel.findOne({
+      threadOwnerId: userId,
+    });
+
+    if (!foundThreadInfo) {
+      throw new Error("Error finding thread");
+    }
+
+    const foundContents = await this.contentsModel.findOne({ _id: foundThreadInfo.contentsId });
+
+    if (!foundContents) {
+      throw new Error("Unable to find thread contents");
+    }
+
+    return foundContents;
   }
 }
